61adf4bf43c8623f25ba2e58c54a70dc17604c70
Sandeep LS <110336567+SandeepLS@users.noreply.github.com>
2023-09-17 10:46:46 +0530

    Update index.js

--- MERN(Register(Post))/index.js
+++ MERN(Register(Post))/index.js
@@ -36,4 +36,22 @@ app.post('/register', async (req,res) => {
 //     res.json({name,email,password});
 // });
 
+
+/* Login*/
+app.post('/login', async (req,res) => {
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if(userDoc){
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if(passOk){
+            
+            res.cookie('token', '').json('pass ok');
+        }else{
+            res.status(422).json('pass not ok');
+        }
+    }else{
+        res.json('not found');
+    }
+});
+
 app.listen(4000); 
